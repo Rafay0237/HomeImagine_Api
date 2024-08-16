4b6eb9d8b7805351437fc53dffb895d2cb51d683
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-16 22:47:38 +0500

    minor naming issue server crash

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function dbConnection() {
 
 dbConnection().catch((error) => console.log(error));
 
-server.listen(PORT, HOST, () => {
+app.listen(PORT, HOST, () => {
   console.log(`Server is listening on ${HOST}:${PORT}`);
 });
 
